ee6e4fb94d0eb9633d9b3f2c5f9312308ed42093
Ryan Shea <ryan@shea.io>
2015-08-29 18:33:02 -0400

    strip main.js down to just the ECPrivateKey export; create a testing suite

--- main.js
+++ main.js
@@ -15,54 +15,6 @@ var ECPrivateKey = asn1.define('ECPrivateKey', function() {
   )
 })
 
-/******
- * Hand-crafted Private Key
- ******/
-
-var publicKeyHex = '04147b79e9e1dd3324ceea115ff4037b6c877c73777131418bfb2b713effd0f502327b923861581bd5535eeae006765269f404f5f5c52214e9721b04aa7d040a75',
-    privateKeyHex = '844055cca13efd78ce79a4c3a4c5aba5db0ebeb7ae9d56906c03d333c5668d5b',
-    options =  {label: 'EC PRIVATE KEY'}
-var publicKeyBitstr = { data: new Buffer(publicKeyHex, 'hex'), unused: 0 }
-var encodedPrivateKey = ECPrivateKey.encode({
-    version: new BN(1),
-    privateKey: new Buffer(privateKeyHex, 'hex'),
-    parameters: '1.3.132.0.10'.split('.'),
-    publicKey: publicKeyBitstr
-}, 'pem', options)
-var decodedPrivateKey = ECPrivateKey.decode(encodedPrivateKey, 'pem', options)
-
-/******
- * OpenSSL Private Key
- ******/
-
-var openSSLEncodedPrivateKey = '-----BEGIN EC PRIVATE KEY-----\n' +
-'MHQCAQEEIIRAVcyhPv14znmkw6TFq6XbDr63rp1WkGwD0zPFZo1boAcGBSuBBAAK\n' +
-'oUQDQgAEFHt56eHdMyTO6hFf9AN7bId8c3dxMUGL+ytxPv/Q9QIye5I4YVgb1VNe\n' +
-'6uAGdlJp9AT19cUiFOlyGwSqfQQKdQ==\n' +
-'-----END EC PRIVATE KEY-----'
-var openSSLDecodedPrivateKey = ECPrivateKey.decode(openSSLEncodedPrivateKey, 'pem', options)
-var openSSLReencodedPrivateKey = ECPrivateKey.encode(openSSLDecodedPrivateKey, 'pem', options)
-var openSSLDecodedPrivateKeyHex = openSSLDecodedPrivateKey.privateKey.toString('hex')
-var openSSLDecodedPublicKeyHex = openSSLDecodedPrivateKey.publicKey.data.toString('hex')
-
-/******
- * Printing
- ******/
-
-console.log('\n\n**************\nHand-crafted Private Key\n**************')
-console.log('\nEncoded private key:')
-console.log(encodedPrivateKey)
-console.log('\nDecoded private key:')
-console.log(decodedPrivateKey)
-
-console.log('\n\n**************\nOpenSSL Private Key\n**************')
-console.log('\nOriginal encoded private key:')
-console.log(openSSLEncodedPrivateKey)
-console.log('\nDecoded private key:')
-console.log(openSSLDecodedPrivateKey)
-console.log('\nDecoded private Key (Hex):')
-console.log(openSSLDecodedPrivateKeyHex)
-console.log('\nDecoded public key (Hex):')
-console.log(openSSLDecodedPublicKeyHex)
-console.log('\nRe-encoded private key:')
-console.log(openSSLReencodedPrivateKey)
+module.exports = {
+    ECPrivateKey: ECPrivateKey
+}
---
+++ test.js
@@ -0,0 +1,33 @@
+var test = require('tape'),
+    ECPrivateKey = require('./main').ECPrivateKey,
+    BN = require('bn.js')
+
+test('encodeAndDecodeSECP256k1PEM', function(t) {
+    t.plan(3)
+
+    var publicKeyHex = '04147b79e9e1dd3324ceea115ff4037b6c877c73777131418bfb2b713effd0f502327b923861581bd5535eeae006765269f404f5f5c52214e9721b04aa7d040a75',
+        privateKeyHex = '844055cca13efd78ce79a4c3a4c5aba5db0ebeb7ae9d56906c03d333c5668d5b',
+        pemOptions =  {label: 'EC PRIVATE KEY'},
+        secp256k1Parameters = [ 1, 3, 132, 0, 10 ],
+        openSSLPrivateKeyPEM = '-----BEGIN EC PRIVATE KEY-----\n' +
+        'MHQCAQEEIIRAVcyhPv14znmkw6TFq6XbDr63rp1WkGwD0zPFZo1boAcGBSuBBAAK\n' +
+        'oUQDQgAEFHt56eHdMyTO6hFf9AN7bId8c3dxMUGL+ytxPv/Q9QIye5I4YVgb1VNe\n' +
+        '6uAGdlJp9AT19cUiFOlyGwSqfQQKdQ==\n' +
+        '-----END EC PRIVATE KEY-----'
+
+    var privateKeyObject = {
+        version: new BN(1),
+        privateKey: new Buffer(privateKeyHex, 'hex'),
+        parameters: secp256k1Parameters,
+        publicKey: { unused: 0, data: new Buffer(publicKeyHex, 'hex') }
+    }
+
+    var privateKeyPEM = ECPrivateKey.encode(privateKeyObject, 'pem', pemOptions)
+    t.equal(privateKeyPEM, openSSLPrivateKeyPEM)
+
+    var privateKeyObject2 = ECPrivateKey.decode(privateKeyPEM, 'pem', pemOptions)
+    t.equal(JSON.stringify(privateKeyObject), JSON.stringify(privateKeyObject2))
+
+    var openSSLPrivateKeyObject = ECPrivateKey.decode(openSSLPrivateKeyPEM, 'pem', pemOptions)
+    t.equal(JSON.stringify(privateKeyObject), JSON.stringify(openSSLPrivateKeyObject))
+})
