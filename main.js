3b39f985a6a009086824b2431fffa0e6b63f34a7
Ryan Shea <ryan@shea.io>
2015-08-31 11:38:32 -0400

    add an SECP256k1PrivateKey object that supports conversion from hex to PEM and vice versa

--- main.js
+++ main.js
@@ -11,6 +11,36 @@ var ECPrivateKey = asn1.define('ECPrivateKey', function() {
   )
 })
 
+function SECP256k1PrivateKey() {
+}
+
+SECP256k1PrivateKey.ec = new EC('secp256k1')
+SECP256k1PrivateKey.parameters = [1, 3, 132, 0, 10]
+SECP256k1PrivateKey.pemOptions = {label: 'EC PRIVATE KEY'}
+
+SECP256k1PrivateKey.hexToPEM = function(privateKeyHex, hexPublicKey) {
+    var privateKeyObject = {
+        version: new BN(1),
+        privateKey: new Buffer(privateKeyHex, 'hex'),
+        parameters: this.parameters
+    }
+
+    if (hexPublicKey) {
+        privateKeyObject.publicKey = {
+            unused: 0,
+            data: new Buffer(hexPublicKey, 'hex')
+        }
+    }
+
+    return ECPrivateKey.encode(privateKeyObject, 'pem', this.pemOptions)
+}
+
+SECP256k1PrivateKey.PEMToHex = function(privateKeyPEM) {
+    var privateKeyObject = ECPrivateKey.decode(privateKeyPEM, 'pem', this.pemOptions)
+    return privateKeyObject.privateKey.toString('hex');
+}
+
 module.exports = {
-    ECPrivateKey: ECPrivateKey
+    ECPrivateKey: ECPrivateKey,
+    SECP256k1PrivateKey: SECP256k1PrivateKey
 }
--- test.js
+++ test.js
@@ -1,19 +1,25 @@
 var test = require('tape'),
-    ECPrivateKey = require('./main').ECPrivateKey,
+    main = require('./main'),
+    ECPrivateKey = main.ECPrivateKey,
+    SECP256k1PrivateKey = main.SECP256k1PrivateKey,
     BN = require('bn.js')
 
-test('encodeAndDecodeSECP256k1PEM', function(t) {
+var privateKeyHex = '844055cca13efd78ce79a4c3a4c5aba5db0ebeb7ae9d56906c03d333c5668d5b',
+    publicKeyHex = '04147b79e9e1dd3324ceea115ff4037b6c877c73777131418bfb2b713effd0f502327b923861581bd5535eeae006765269f404f5f5c52214e9721b04aa7d040a75',
+    openSSLPrivateKeyPEM = '-----BEGIN EC PRIVATE KEY-----\n' +
+    'MHQCAQEEIIRAVcyhPv14znmkw6TFq6XbDr63rp1WkGwD0zPFZo1boAcGBSuBBAAK\n' +
+    'oUQDQgAEFHt56eHdMyTO6hFf9AN7bId8c3dxMUGL+ytxPv/Q9QIye5I4YVgb1VNe\n' +
+    '6uAGdlJp9AT19cUiFOlyGwSqfQQKdQ==\n' +
+    '-----END EC PRIVATE KEY-----',
+    openSSLPrivateKeyPEMCompact = '-----BEGIN EC PRIVATE KEY-----\n' +
+    'MC4CAQEEIIRAVcyhPv14znmkw6TFq6XbDr63rp1WkGwD0zPFZo1boAcGBSuBBAAK\n' +
+    '-----END EC PRIVATE KEY-----'
+
+test('testECPrivateKey', function(t) {
     t.plan(3)
 
-    var privateKeyHex = '844055cca13efd78ce79a4c3a4c5aba5db0ebeb7ae9d56906c03d333c5668d5b',
-        publicKeyHex = '04147b79e9e1dd3324ceea115ff4037b6c877c73777131418bfb2b713effd0f502327b923861581bd5535eeae006765269f404f5f5c52214e9721b04aa7d040a75',
-        secp256k1Parameters = [ 1, 3, 132, 0, 10 ],
-        pemOptions =  {label: 'EC PRIVATE KEY'},
-        openSSLPrivateKeyPEM = '-----BEGIN EC PRIVATE KEY-----\n' +
-        'MHQCAQEEIIRAVcyhPv14znmkw6TFq6XbDr63rp1WkGwD0zPFZo1boAcGBSuBBAAK\n' +
-        'oUQDQgAEFHt56eHdMyTO6hFf9AN7bId8c3dxMUGL+ytxPv/Q9QIye5I4YVgb1VNe\n' +
-        '6uAGdlJp9AT19cUiFOlyGwSqfQQKdQ==\n' +
-        '-----END EC PRIVATE KEY-----'
+    var secp256k1Parameters = [ 1, 3, 132, 0, 10 ],
+        pemOptions =  {label: 'EC PRIVATE KEY'}
 
     var privateKeyObject = {
         version: new BN(1),
@@ -30,4 +36,17 @@ test('encodeAndDecodeSECP256k1PEM', function(t) {
 
     var openSSLPrivateKeyObject = ECPrivateKey.decode(openSSLPrivateKeyPEM, 'pem', pemOptions)
     t.equal(JSON.stringify(privateKeyObject), JSON.stringify(openSSLPrivateKeyObject))
+})
+
+test('testSECP256k1PrivateKey', function(t) {
+    t.plan(3)
+
+    var privateKeyPEM = SECP256k1PrivateKey.hexToPEM(privateKeyHex, publicKeyHex)
+    t.equal(privateKeyPEM, openSSLPrivateKeyPEM)
+
+    var privateKeyPEMCompact = SECP256k1PrivateKey.hexToPEM(privateKeyHex)
+    t.equal(privateKeyPEMCompact, openSSLPrivateKeyPEMCompact)
+
+    var decodedPrivateKeyHex = SECP256k1PrivateKey.PEMToHex(privateKeyPEM)
+    t.equal(decodedPrivateKeyHex, privateKeyHex)
 })
